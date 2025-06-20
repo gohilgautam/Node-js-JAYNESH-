bc38d2229ebfb233f7b098a7cbc1cc2674459ce4
Gautam Gohil <gohilgautam2405@gmail.com>
2025-06-20 15:24:24 +0530

    API Student

---
+++ routes/userRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const route = express.Router();
+
+console.log("Routing...");
+
+const { fetchUser, insertUser,deleteUser,updateUsers } = require("../controllers/usersController");
+
+//Fetch Users
+route.get("/users", fetchUser);
+
+//Insert User
+route.post("/users", insertUser);
+
+//Delete User
+route.delete("/users/:id", deleteUser);
+
+//Update User
+route.patch("/users/:id" , updateUsers);
+
+// students route
+
+route.use("/student", require("./studRoute"));
+
+module.exports = route;
